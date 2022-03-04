98a895d16c0fb80ecbd566b460bdafd3424e8f88
AlejandroPino <92079996+AlejandroPino@users.noreply.github.com>
2022-03-04 10:11:37 +0100

    Retocada la vista Tema
    He mejorado la vista Tema, queda pendiente (opcional) encontrar la solución para no poder introducir un número de tema existente mediante un select

--- TestLaravel/resources/views/profesor/tema.blade.php
+++ TestLaravel/resources/views/profesor/tema.blade.php
@@ -1,36 +1,88 @@
-
-
 @extends('layouts.app')
 @section('content')
 
-<h1>TEMA</h1>
 
-@foreach (Auth::user()->materias->temas as $tema)
+<div class="container">
 
-<p>Tema {{ $tema->numero }}: {{ $tema->nombre }} </p>
-
-@endforeach
-
-
-
-<form action="{{route('tema.store')}}" method="post">
-    @csrf
-    <div class="form-group">
-        <label for="numero">Tema numero</label>
-        <input type="text" name="numero" id="numero" class="form-control" value="{{old('numero')}}">
+    <!-- LISTA DE TEMAS -->
+    <div class="row mt-5">
+        <div class="col-3"></div>
+        <div class="col">
+            <h1>Lista de temas</h1>
+        </div>
+        <div class="col-3"></div>
     </div>
 
-    <div class="form-group">
-        <label for="nombre">Tema nombre</label>
-        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old('nombre')}}">
+    <div class="row mt-3">
+        <div class="col-3"></div>
+        <div class="col">
+            <table class="table table-sm table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Tema</th>
+                        <th scope="col">Nombre</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    @foreach (Auth::user()->materias->temas as $tema)
+                    <tr>
+                        <th scope="row">{{$tema->numero}}</th>
+                        <td>{{$tema->nombre}}</td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+        <div class="col-3"></div>
     </div>
 
+    <!-- AÑADIR TEMA -->
+    <div class="row mt-5">
+        <div class="col-3"></div>
+        <div class="col">
+            <h1>Añadir tema</h1>
+        </div>
+        <div class="col-3"></div>
+    </div>
 
+    <div class="row mt-4">
 
+        <div class="col-3"></div>
+        <div class="col">
 
+            <form action="{{route('tema.store')}}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="numero"><h5>Numero del tema</h5></label>
+                    <input type="text" name="numero" id="numero" class="form-control" value="{{old('numero')}}">
 
-    <button type="submit" class="btn btn-primary">Añadir tema</button>
-</form>
+                    <!-- MÉTODO PARA NO INTRODUCIR UN TEMA REPETIDO -->
 
+                    <!-- <select>
+                        @for($i = $tema->last()->numero, $i < 30; $i++; );
+                        <option value="{{ $i }}">{{ $i }}</option>
+                        @endfor
+                    </select> -->
+
+                    
+
+                </div>
+
+                <div class="form-group mt-4">
+                    <label for="nombre"><h5>Nombre del tema</h5></label>
+                    <input type="text" name="nombre" id="nombre" class="form-control" value="{{old('nombre')}}">
+                </div>
+
+                <button type="submit" class="btn btn-primary mt-4">Añadir tema</button>
+
+            </form>
+
+        </div>
+        <div class="col-3"></div>
+    </div>
+
+</div>
 
 @endsection

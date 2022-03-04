3f59576e724617da89ceb4306207c92601696fe9
AlejandroPino <92079996+AlejandroPino@users.noreply.github.com>
2022-03-04 09:44:03 +0100

    traducciones Layouts/App.blade.php

--- TestLaravel/resources/views/layouts/app.blade.php
+++ TestLaravel/resources/views/layouts/app.blade.php
@@ -39,13 +39,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarme') }}</a>
                                 </li>
                             @endif
                         @else
